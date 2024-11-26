e48f261e5e8572e1b302c71ca27b7b7218305238
SangJin983 <lazistar983@gmail.com>
2024-11-26 20:16:05 +0900

    Initialize view model with pagination and post rendering logic

--- ViewModel.js
+++ ViewModel.js
@@ -1,13 +1,51 @@
-import { PostModel } from "./Model";
-import { renderPosts } from "./VIew";
+import { PostModel, CurrentPageState, CurrentPaginationState } from "./Model";
+import { PostView, PaginationView } from "./VIew";
+import { range } from "rxjs";
 
 export const createViewModel = () => {
   const postModel = new PostModel();
-  const postsRenderer = renderPosts();
+  const currentPageState = new CurrentPageState();
+  const currentPaginationState = new CurrentPaginationState();
+  const postView = new PostView();
+  const paginationView = new PaginationView();
+
+  const POST_PER_PAGE = 5;
+  const PAGINATION_RANGE = 3;
+  const PAGINATION_END = 20;
 
   const initApp = () => {
-    postModel.subscribe(postsRenderer.render);
-    postModel.fetch(0, 5);
+    const updatePosts = (pageNumber) => {
+      const start = (pageNumber - 1) * POST_PER_PAGE;
+      postModel.fetch(start, POST_PER_PAGE);
+    };
+
+    const updatePaginationRange = (pageNumber) => {
+      const startPage = Math.max(1, pageNumber - PAGINATION_RANGE);
+      const endPage = Math.min(PAGINATION_END, pageNumber + PAGINATION_RANGE);
+      const totalPage = endPage - startPage + 1;
+
+      const currentRange = range(startPage, totalPage);
+      currentPaginationState.setRange(currentRange);
+    };
+
+    paginationView.onPageClick((pageNumber) => {
+      currentPageState.setPage(pageNumber);
+    });
+
+    currentPageState.subscribe((pageNumber) => {
+      updatePosts(pageNumber);
+      updatePaginationRange(pageNumber);
+    });
+
+    postModel.subscribe((posts) => postView.render(posts));
+
+    currentPaginationState.subscribe((range) => {
+      const currentPage = currentPageState.getPage();
+      paginationView.render(range);
+      paginationView.highlight(currentPage);
+    });
+
+    currentPageState.setPage(1);
   };
 
   return {
